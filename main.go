de1091a91d9201f8b60fc31bf0af6aff255f50cb
Jairaj Sahgal <jairajiitaspirant@gmail.com>
2023-10-15 13:17:24 +0530

    slicing in go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var bookings [50]string
+	var bookings = []string{}
 
 	var firstName string
 	var lastName string
@@ -33,12 +33,9 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value of array: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v", len(bookings))
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
